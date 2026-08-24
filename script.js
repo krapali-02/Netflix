3847dc34a1b6606331344a470c12dd15ae33db14
Krapali Rathor <krapali.rathor1@gmail.com>
2026-08-25 00:43:58 +0530

    addes event listener

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const trendContent = document.querySelector(".trend-content");
 const btnRight = document.querySelector(".scroll-btn-right")
 const btnLeft = document.querySelector(".scroll-btn-left")
+
+btnRight.addEventListener("click",() =>{
+    trendContent.scrollBy({
+        left: 800,
+        behavior: "smooth"
+    })
+})
+
+btnLeft.addEventListener("click",()=>{
+    trendContent.scrollBy({
+        left: -800,
+        behavior : "smooth"
+    })
+})
